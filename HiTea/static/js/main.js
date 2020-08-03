80ea58336d4a851c9e01d4f62b4be79f3a643398
unknown <jimmyy1337@gmail.com>
2020-08-03 02:11:01 -0400

    Updated down arrow to relative units and scrollspy offset

--- HiTea/static/js/main.js
+++ HiTea/static/js/main.js
@@ -1,2 +1,7 @@
 M.AutoInit();
 
+// Overwrite scrollOffset option for scrollspy to 0px
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.scrollspy');
+    var instances = M.ScrollSpy.init(elems, {scrollOffset: 0});
+  });
