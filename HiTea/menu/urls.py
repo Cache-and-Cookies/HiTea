afb76231630bd0492327356366f4aaaf8380fdda
unknown <jimmyy1337@gmail.com>
2020-08-04 23:13:43 -0400

    cahche

--- HiTea/menu/urls.py
+++ HiTea/menu/urls.py
@@ -3,9 +3,5 @@ from django.urls import path
 from . import views as menu_views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('', menu_views.getMenuData, name='menu-home'),
-=======
     path('', menu_views.getMenuData, name='menu-search'),
->>>>>>> master
 ]
