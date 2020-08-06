988386c41020b73f3ac27874857631018a8db554
DinosaurusRexia <clark.li325@gmail.com>
2020-08-05 20:24:45 -0400

    small fixes

--- HiTea/menu/urls.py
+++ HiTea/menu/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from . import views as menu_views
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', menu_views.getMenuData, name='menu-search'),
-=======
     path('', menu_views.getMenuData, name='menu-home'),
->>>>>>> menu
 ]
