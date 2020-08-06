e0ba2fa1606149f94407eccf46104aba317f9dc6
unknown <jimmyy1337@gmail.com>
2020-08-05 22:43:02 -0400

    Updated all modals style with topping listings

--- HiTea/menu/urls.py
+++ HiTea/menu/urls.py
@@ -4,8 +4,4 @@ from . import views as menu_views
 
 urlpatterns = [
     path('', menu_views.getMenuData, name='menu-search'),
-<<<<<<< HEAD
-    path('', menu_views.getMenuData, name='menu-home'),
-=======
->>>>>>> menu
 ]
